573dad1b1c6590114b9a96df6dbf6f01ad5276b9
Isuru <isuruindrajith680@gmail.com>
2025-10-05 13:40:02 +0530

    Initial commit - SpendSmart Expense Tracker

---
+++ Models/Expense.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpendSmart.Models
+{
+
+//these classes/modells will be later stored in the database
+    public class Expense
+    {
+        public int Id { get; set; }
+
+        public decimal Value { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+//? means we cannot have null values for this property
+    }
+}
